a12162ef30eba8ecaa87bc518fb7e0c3bc9f3d53
NiklasMelton <niklasmelton@gmail.com>
2024-03-07 14:43:55 -0600

    update init file

--- __init__.py
+++ __init__.py
@@ -2,14 +2,18 @@ from elementary.BaseART import BaseART
 from elementary.ART1 import ART1
 from elementary.ART2 import ART2A
 from elementary.BayesianART import BayesianART
-from elementary.GaussianART import GaussianART
+from elementary.DualVigilanceART import DualVigilanceART
 from elementary.EllipsoidART import EllipsoidART
+from elementary.GaussianART import GaussianART
+from elementary.FuzzyART import FuzzyART
 from elementary.HypersphereART import HypersphereART
 from elementary.QuadraticNeuronART import QuadraticNeuronART
+
 
 from supervised.ARTMAP import ARTMAP, SimpleARTMAP
 
 from hierarchical.SMART import SMART
+from hierarchical.DeepARTMAP import DeepARTMAP
 
 from fusion.FusionART import FusionART
 
@@ -26,10 +30,14 @@ __all__ = [
     "EllipsoidART",
     "HypersphereART",
     "QuadraticNeuronART",
+    "FuzzyART",
+    "TopoART",
+    "DualVigilanceART",
     "ARTMAP",
     "SimpleARTMAP",
+    "DeepARTMAP",
     "SMART",
     "FusionART",
     "BARTMAP",
-    "TopoART",
+
 ]

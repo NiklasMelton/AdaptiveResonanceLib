64b5827d053e28dd2099d8e4ff642658765f177e
niklas melton <niklasmelton@gmail.com>
2024-03-11 19:29:13 -0500

    test ART1

---
+++ examples/test_art1.py
@@ -0,0 +1,40 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import path
+import sys
+
+# directory reach
+directory = path.Path(__file__).abspath()
+
+print(directory.parent)
+# setting path
+sys.path.append(directory.parent.parent)
+
+from elementary.ART1 import ART1
+from common.utils import normalize
+
+
+def cluster_blobs():
+    data, target = make_blobs(n_samples=150, centers=3, cluster_std=0.50, random_state=0, shuffle=False)
+    data =  ( data > 0.5).astype(int)
+    print("Data has shape:", data.shape)
+
+    X = normalize(data)
+    print("Prepared data has shape:", X.shape)
+
+    params = {
+        "rho": 0.7,
+        "beta": 1.0,
+        "L": 1.0
+    }
+    cls = ART1(params)
+    y = cls.fit_predict(X)
+
+    print(f"{cls.n_clusters} clusters found")
+
+    cls.visualize(X, y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    cluster_blobs()

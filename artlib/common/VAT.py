7b439b325dc59e67b48f58861820ec1f24e87ced
niklas melton <niklasmelton@gmail.com>
2024-09-09 13:01:30 -0500

    add VAT

---
+++ artlib/common/VAT.py
@@ -0,0 +1,31 @@
+import numpy as np
+from typing import Optional, Tuple, Callable
+from scipy.spatial.distance import pdist, squareform
+
+
+def VAT(data: np.ndarray, distance_metric: Optional[Callable] = lambda X: pdist(X, "euclidean")) -> Tuple[np.ndarray, np.ndarray]:
+    if distance_metric is None:
+        R = data
+    else:
+        R = squareform(distance_metric(data))
+
+    dim = data.shape[0]
+    P = np.zeros((dim,), dtype=int)
+
+    i, j = np.unravel_index(np.argmax(R), R.shape)
+    P[0] = i
+    I = [i]
+    J = set(range(dim)) - {i}
+
+    for r in range(1, dim):
+        J_list = list(J)
+        R_sub = R[np.ix_(I, J_list)]
+        _, j_ = np.unravel_index(np.argmin(R_sub), R_sub.shape)
+        j = J_list[j_]
+        P[r] = j
+        I.append(j)
+        J -= {j}
+
+    return R[np.ix_(P, P)], P
+
+
---
+++ examples/test_VAT.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+from artlib import VAT
+import time
+
+
+def visualize_blobs():
+    data, target = make_blobs(n_samples=2000, centers=3, cluster_std=0.50, random_state=0, shuffle=False)
+    print("Data has shape:", data.shape)
+
+    t0 = time.time()
+    R, P = VAT(data)
+    dt = time.time() - t0
+    print("time:", dt)
+    print(P.shape, np.unique(P))
+
+
+    plt.figure()
+    plt.scatter(data[:,0], data[:,1],c=P, cmap="jet",s=10)
+
+    plt.figure()
+    plt.imshow(R)
+    plt.show()
+
+if __name__ == "__main__":
+
+    visualize_blobs()

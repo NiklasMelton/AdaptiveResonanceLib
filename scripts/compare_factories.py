1908c68c4660197127698d766f595fba1ba35ede
niklas melton <niklasmelton@gmail.com>
2025-09-04 02:15:41 -0500

    add gpu tests

--- scripts/compare_factories.py
+++ scripts/compare_factories.py
@@ -119,7 +119,7 @@ def time_call(label, fn, *args, **kwargs):
 # ------------------------------
 # Hyperparams from your sample
 RHO = 0.8
-H_RHO = 0.6
+H_RHO = 0.7
 G_RHO = 1e-90
 ALPHA = 1e-10
 BETA = 1.0

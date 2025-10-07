36752409fcf28f4d878cd4495c2281ae5675c975
Niklas Melton <niklasmelton@gmail.com>
2025-10-07 15:59:11 -0500

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ def setup(app):
 project = 'AdaptiveResonanceLib'
 copyright = '2025, Niklas Melton'
 author = 'Niklas Melton'
-release = '0.1.6'
+release = '0.1.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

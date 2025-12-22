1a37c96a7499fcc43b89fd11ac586adec500b8b5
niklas melton <niklasmelton@gmail.com>
2025-12-21 18:05:44 -0600

    bug fixes

--- artlib/elementary/BinaryFuzzyART.py
+++ artlib/elementary/BinaryFuzzyART.py
@@ -271,8 +271,8 @@ class BinaryFuzzyART(FuzzyART):
                 ]
             if rows:
                 T_num, T_den, T_cache, T_idx = map(tuple, zip(*rows))
-                T_num = np.ascontiguousarray(T_num)
-                T_den = np.ascontiguousarray(T_den)
+                T_num = np.ascontiguousarray(T_num, dtype=np.unit32)
+                T_den = np.ascontiguousarray(T_den, dtypie=np.uint32)
                 order = fracsort(T_num, T_den)
             else:
                 T_cache = T_idx = order = ()

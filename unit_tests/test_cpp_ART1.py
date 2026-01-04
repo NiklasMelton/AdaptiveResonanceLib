f808be4d0de090537dfd255e2246f6bb7f92b2aa
niklas melton <niklasmelton@gmail.com>
2026-01-04 00:44:40 -0600

    create cpp ART1 and unit tests

--- unit_tests/test_cpp_ART1.py
+++ unit_tests/test_cpp_ART1.py
@@ -12,7 +12,7 @@ def test_prepare_data():
         random_state=0,
         shuffle=False,
     )
-    data = binarize_features_thermometer(data, 4).astype(np.bool)
+    data = binarize_features_thermometer(data, 4).astype(np.int)
     params = {"rho": 0.8, "L": 1.0}
     A = pyART1(**params)
     B = cppART1(**params)
@@ -31,7 +31,7 @@ def test_consistency():
             random_state=0,
             shuffle=False,
         )
-    data = binarize_features_thermometer(data, 4).astype(np.bool)
+    data = binarize_features_thermometer(data, 4).astype(np.int)
     params = {"rho":0.8, "L":1.0}
     A = pyART1(**params)
     B = cppART1(**params)
